47015ec29d2a934bb8277a3f163f26f6b8603cff
unknown <ranausamauet3430@gmail.com>
2021-01-28 14:21:00 +0500

    errors resolved

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,18 +4,8 @@ import './Contact.css'
 function Contact() {
  return (
   <>
-   <div className='contact-heading'>
-     <h5>Contact Us!</h5>
-     
-   </div>
-   <div className='contact-form-container'>
-    <i class="far fa-envelope-open"></i>
-    <input type='email' className='contact-message-input'name='email'placeholder='Write to us (Your message)'></input>
-    <input type='email' className='contact-email-input'name='email'placeholder='Your email'></input>
-    <input type='email' className='contact-address-input'name='email'placeholder='Your address'></input>
-
-    <button type='submit'  className="send-button" name='button'>Send</button>
-    <h8>BUY IT © 2021</h8>
+   <div>
+  <h1>contact us!</h1>
    </div>
   </>
  )
